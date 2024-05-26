7e99ecb9330c87b637dd078d5fb75b1878c936b8
NezBut <xyi12312354@mail.ru>
2024-05-27 00:29:37 +0700

    update start

--- start.sh
+++ start.sh
@@ -9,4 +9,8 @@ export PYTHONPATH=$path
 
 cd $project_path
 
+export TG_BOT_TOKEN="TG BOT TOKEN" # you can get it from @BotFather
+export DATABASE_URI="postgresql+asyncpg://USERNAME:PASSWORD@localhost:5433/bot_anketa"
+export KV_DATABASE_URI="redis://localhost:6379/4"
+
 poetry run tgbot
